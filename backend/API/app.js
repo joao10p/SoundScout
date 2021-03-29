ba43fd4d1eaae7ce4531a5d0cacc08bc101f0a64
PedroMM-G <a92930@alunos.uminho.pt>
2021-03-29 11:56:48 +0100

    fs

--- backend/API/app.js
+++ backend/API/app.js
@@ -1,3 +1,154 @@
+
+
+
+
+
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressSanitizer = require('express-sanitizer');
+const session = require('express-session');
+const mainRoutes = require('./routes/main.routes.js');
+const helmet = require('helmet');
+const contentSecurityPolicy = require("helmet-csp");
+const csrf = require('csurf');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const models = require('./models/');
+const flash = require('express-flash');
+const http = require('http');
+const fs = require('fs');
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: 'our super secret session secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 3600000,
+    secure: true,
+    httpOnly: true
+  }
+}));
+ const server = http.createServer(function(req,res){
+    res.writeHead(200,{ 'Content-Type': 'text/html'})
+    fs.readFile('index.html',function(error,data){
+      if(error){
+        res.writeHead(404)
+        res.write('Error: File Not Found')
+      }else {
+        res.write(data);
+      }
+      res.end();
+    })
+  });
+
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
+
+app.use('/', mainRoutes);
+
+
+
+app.use(
+  contentSecurityPolicy({
+    defaultSrc: ["'self'"],
+    scriptSrc: ['*.google-analytics.com'],
+    styleSrc: ["'unsafe-inline'"],
+    imgSrc: ['*.google-analytics.com'],
+    connectSrc: ["'none'"],
+    fontSrc: [],
+    objectSrc: [],
+    mediaSrc: [],
+    frameSrc: []
+  })
+);
+
+
+app.listen(port, function() {
+  console.log('Example app listening on port ' + port + '!');
+});
+
+//app.use(expressValidator());
+app.use(cors());
+app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    var randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2, randomNumber.length);
+    res.cookie('cookieName', randomNumber, {
+      maxAge: 900000,
+      httpOnly: true,
+      secure: true
+    });
+    console.log('cookie created successfully');
+  }
+  else {
+    console.log('cookie exists', cookie);
+  }
+  next();
+});
+
+app.use(function(req, res, next) {
+  for (var item in req.body) {
+  }
+  next();
+});
+
+
+app.use(function(req, res, next) {
+  // check if session exists
+  if (global.sessData === undefined) {
+    global.sessData = req.session;
+    global.sessData.ID = req.sessionID;
+  }
+  else { // yes, cookie was already present
+    console.log('session exists', global.sessData.ID);
+  }
+  next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+require('./routes/auth.routes.js')(app, passport);
+require('./config/passport/passport.js')(passport, models.user);
+//Sync Database
+models.sequelize.sync().then(function() {
+  console.log('Nice! Database looks fine');
+
+}).catch(function(err) {
+  console.log(err, "Something went wrong with the Database Update!");
+});
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*const express = require('express');
 const app = express();
 const http = require("http");
@@ -120,7 +271,7 @@ app.use(function(req, res, next) {
 
   module.exports = app;
   */
-  const hostname = process.env.HOST || '127.0.0.1';
+  /*const hostname = process.env.HOST || '127.0.0.1';
   const port = process.env.PORT || 3000;
   //const homepage = require('../../startbootstrap-grayscale-gh-pages/index.html');
   const express = require('express');
@@ -143,7 +294,7 @@ app.use(function(req, res, next) {
   const flash = require('express-flash');
   const http = require('http');
   
-  const server = http.createServer(function(req,res){
+ /* const server = http.createServer(function(req,res){
     res.writeHead(200,{ 'Content-Type': 'text/html'})
     fs.readFile('index.html',function(error,data){
       if(error){
@@ -178,11 +329,11 @@ app.use(function(req, res, next) {
     }
 });
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+}); 
   
   app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
   app.use(expressSanitizer());
