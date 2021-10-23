c8c1e42878da38bd0c8574401c074af5f6f766aa
PedroMM-G <a92930@alunos.uminho.pt>
2021-10-24 00:33:57 +0100

    NAO TOU A PERCEBER ESTA MERDA

--- Backend/views/menu_controller.js
+++ Backend/views/menu_controller.js
@@ -1,3 +1,6 @@
+
+alert("Hello! I am an alert box!!");
+/*
 window.onload = function() {
     document.getElementById("confirmar").onclick = function(e) {
         concol();
@@ -70,3 +73,4 @@ window.onload = function() {
 
 
 
+*/
