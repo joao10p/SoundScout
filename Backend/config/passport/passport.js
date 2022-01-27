9df8e0a9aeef42fa28195bd63b8885cafd936b79
duartebrandao <79524904+duartebrandao@users.noreply.github.com>
2022-01-27 16:39:01 +0000

    LOGIN POSTMAN DONE!!!!!!!!

--- Backend/config/passport/passport.js
+++ Backend/config/passport/passport.js
@@ -39,8 +39,9 @@ module.exports = function(passport, user) {
           var data = {
             nome: req.body.nome,
             numero: req.body.numero,
-            email: email,
+            email: req.body.email,
             cargo: req.body.cargo,
+            username: req.body.username,
             password: userPassword
            
             
@@ -57,23 +58,34 @@ module.exports = function(passport, user) {
       });
     }
   ));
-  
-  passport.use(new LocalStrategy(
+  /*
+  passport.use('local-signin',new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+
+  },
   function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
+    var User = user;
+    var isValidPassword = function(userpass, password) {
+      return bCrypt.compareSync(password, userpass);
+
+    }
+    User.findOne({ where: {username: username }}).then(function(err, user) {
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-      if (!user.validPassword(password)) {
+      if (!isValidPassword(user.password, password)) {
         return done(null, false, { message: 'Incorrect password.' });
       }
       return done(null, user);
     });
   }
 ));
+*/
   //LOCAL SIGNIN
-  passport.use('local-signin', new LocalStrategy({
+ passport.use('local-signin', new LocalStrategy({
       // by default, local strategy uses username and password, we will override with email
       usernameField: 'email',
       passwordField: 'password',
@@ -92,10 +104,7 @@ module.exports = function(passport, user) {
         if (!isValidPassword(user.password, password)) {
           return done(null, false, jsonMessages.user.password);
         }
-        var userinfo = user.get();
-        global.sessData.userinfo = userinfo;
-        console.log(userinfo);
-        return done(null, userinfo);
+        return done(null, user);
       }).catch(function(err) {
         console.log("Error:", err);
         return done(null, false, jsonMessages.user.error);
