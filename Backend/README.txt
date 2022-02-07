e95d10535c4d899b5e56b0fc7e000ec59f8bed60
PedroMM-G <a92930@alunos.uminho.pt>
2022-02-07 12:22:27 +0000

    subs quase todas feitas (falta frontend)

--- Backend/README.txt
+++ Backend/README.txt
@@ -4,3 +4,13 @@ FALTA FAZER!!!!!
 2-- Falta consilidar as galerias! 
 3-- Falta fazer a parte do album!
 4-- Fazer Playlist
+
+
+
+
+
+
+
+
+galeria_sound : myFunction nao deixa funcionar as coisas
+equipa_sound e equipa_scout : myFunction nao deixa funcionar as coisas
---
+++ Backend/public/js/revista_id_sound.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+
+    document.getElementById("subs").addEventListener("click", function () {
+        save_subs();
+    });
+
+    function save_subs() {
+        var data = {};
+        data.email = document.getElementById("inputEmail").value;
+        console.log(data);
+
+        fetch('http://localhost:3000/subs/', {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            body: JSON.stringify(data)
+        }).then(function (response) {
+            if (!response.ok) {
+                console.log(response.status); //=> number 100–599
+                console.log(response.statusText); //=> String
+                console.log(response.headers); //=> Headers
+                console.log(response.url); //=> String
+                if (response.status === 409) {
+                    alert("Duplicated occurrences Code");
+                }
+                else {
+                    throw Error(response.statusText);
+                }
+            }
+            else {
+                /*document.getElementById("nome_revistas").reset();
+                 document.getElementById("numero_revistas").reset();
+                 document.getElementById("select_revistas").reset();*/
+                alert("Subscrição feita com sucesso! Está atento ao teu email! :)");
+                //refreshanalise();
+            }
+        }).then(function (result) {
+            console.log(result);
+        }).catch(function (err) {
+            //alert("Submission error");
+            console.error(err);
+        });
+    }
+}
